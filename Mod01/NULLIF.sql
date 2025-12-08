10a7970b6a87d1581183d337484fdad4c780b662
roblevay <108578793+roblevay@users.noreply.github.com>
2025-12-08 03:38:28 -0800

    Add files via upload

---
+++ Mod01/NULLIF.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS budgets
+CREATE TABLE budgets (  
+   dept           TINYINT,  
+   current_year   DECIMAL(10,2),  
+   previous_year  DECIMAL(10,2)  
+);  
+  
+INSERT INTO budgets VALUES(1, 100000, 150000);  
+INSERT INTO budgets VALUES(2, NULL, 300000);  
+INSERT INTO budgets VALUES(3, 0, 100000);  
+INSERT INTO budgets VALUES(4, NULL, 150000);  
+INSERT INTO budgets VALUES(5, 300000, 300000);  
+  
+SELECT dept, NULLIF(current_year,  
+   previous_year) AS LastBudget  
+FROM budgets;
